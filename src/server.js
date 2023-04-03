dadd096f4ca746616713f0a8bb662b11865cb4af
RaphaelFiais <devfiais23@gmail.com>
2023-04-03 10:10:48 -0300

    feat: add express.static

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import routes from './routes'
 import cors from 'cors'
+import { resolve } from 'path'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/image', express.static(resolve(__dirname, '../', 'uploads')))
 routes(app)
 
 app.listen('3000', () => {
