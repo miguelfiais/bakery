c0512606a073d55a5a4b3df04d5b703d7d4156f5
miguelfiais <miguel.fiais10@gmail.com>
2023-08-07 10:44:38 -0300

    feat: add deployment config

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import routes from './routes'
 import cors from 'cors'
 import { resolve } from 'path'
 
+const port = process.env.PORT || 3000
+
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/image', express.static(resolve(__dirname, '../', 'uploads')))
 routes(app)
 
-app.listen('3000', () => {
+app.listen(port, () => {
   console.log('Server started')
 })
